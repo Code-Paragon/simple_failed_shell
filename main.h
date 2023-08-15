7222a909f053bd89ac06a2674d1fb5b90871bbd6
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-08-15 19:28:26 +0100

    add main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int shell(char *, char *const [], size_t, ssize_t, char[], char *[], int, int, int, pid_t);
+
+#endif MAIN_H
