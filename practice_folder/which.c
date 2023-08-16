bfd7191e6f489c2bc1ebba631d964f15b3d974e7
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-08-16 09:29:05 +0100

    practice

--- practice_folder/which.c
+++ practice_folder/which.c
@@ -3,21 +3,45 @@
 #include <string.h>
 
 int main(int ac, char **av, char **env) {
-	char *path;
-	char *token;
-	char *delim = ":";
-	char *file;
+    char *path;
+    char *token;
+    char *delim = ":";
+    char *file;
 
-	path = getenv("PATH");
+    path = getenv("PATH");
+    
+    if (path == NULL) {
+        printf("PATH environment variable not set.\n");
+        return 1;
+    }
 
-	token = strtok(path, delim);
+    char *path_copy = strdup(path);  // Create a copy of PATH to avoid modifying the original
+    if (path_copy == NULL) {
+        perror("Memory allocation error");
+        return 1;
+    }
 
-	while (token != NULL)
-	{
-		token = strtok(NULL, delim);
-		file = strcat(token, "/");
-		file = strcat(token, "ls");
-		printf("%s\n", file);
-	}
-	return (0);
+    token = strtok(path_copy, delim);
+
+    while (token != NULL)
+    {
+        char *file = (char *)malloc(strlen(token) + strlen("/ls") + 1);
+        if (file == NULL) {
+            perror("Memory allocation error");
+            return 1;
+        }
+
+        strcpy(file, token);
+        strcat(file, "/ls");
+
+        printf("%s\n", file);
+
+        free(file);
+
+        token = strtok(NULL, delim);
+    }
+
+    free(path_copy);
+
+    return 0;
 }
