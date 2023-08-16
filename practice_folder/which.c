68a49432cd159c8f6c447a2b1fce5ec1333ad628
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-08-16 08:29:11 +0100

    practice

--- practice_folder/which.c
+++ practice_folder/which.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int ac, char **av, char **env) {
 	char *path;
+	char *token;
+	char *delim = ":";
 
 	path = getenv("PATH");
 
 	printf("%s\n", path);
+
+	token = strtok(path, delim);
+
+	printf("%s", token);
 
 
 	return (0);
