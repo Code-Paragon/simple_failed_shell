fcbc4d43a6ea05686106c906815fcedfa809ed67
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-08-18 13:13:49 +0100

    code test

--- practice_folder/codetest.c
+++ practice_folder/codetest.c
@@ -75,8 +75,8 @@ char *search_file_path(char *command, char *token)
 	strcat(file, command);
 
 	if (stat(file, &sb) == 0){
-		return (file);	
 		printf("%s\n", file);
+		return (file);
 	}
 
 	free(file);
