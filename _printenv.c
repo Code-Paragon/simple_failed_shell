624c964f69effe35ca877d5ba0d937c20eba809d
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-08-21 16:58:17 +0100

    add function description

--- _printenv.c
+++ _printenv.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
-extern char **environ;
-
+/**
+ * printenv - prints the environment variables
+ * 
+ * Return: 0
+ */
 int _printenv(void)
 {
 	char **env;
 
-/* Loop through the environment variables and print them */
-	for (env = environ; *env != NULL; env++) 
+	/* Loop through the environment variables and print them */
+	for (env = environ; *env != NULL; env++)
 	{
 		printf("%s\n", *env);
 	}
