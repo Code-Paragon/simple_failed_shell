6dc0d293a14ec71b9fa86b8984a0fa0736943c81
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-08-21 17:15:49 +0100

    betty

--- _atoi.c
+++ _atoi.c
@@ -11,14 +11,15 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do {
+	do
+	{
 		if (*s == '-')
 			sign *= -1;
 
-		/** 
-		* calculates the corresponding integer value
-		* for digits within 0 and 9 and appends it
-		*/
+		/**
+		 * calculates the corresponding integer value
+		 * for digits within 0 and 9 and appends it
+		 */
 		else if (*s >= '0' && *s <= '9')
 			num = num * 10 + (*s - '0');
 		else if (num > 0)
