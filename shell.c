f914e6b2ba125381fc128ead02e0c3f8222abf62
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-08-24 13:40:14 +0100

    betty

--- shell.c
+++ shell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 int create_process(char *fraginputstr[], char *const envp[]);
 int (*get_Plugin(char *command))(char **args, char **front);
+char **_prompt(char *inputstr, char *fraginputstr[]);
 
 /**
  * shell - the main shell
@@ -14,36 +15,13 @@ int (*get_Plugin(char *command))(char **args, char **front);
  */
 int shell(char *const envp[], char *inputstr,
 	char **args, char __attribute__((__unused__)) **front)
-{
-	ssize_t read = 1, Firstwrite;
-	size_t len = 100;
-	char delim[] = " \n\t";
+{	
 	char *fraginputstr[1000];
-	int y = 0;
 	int (*Plugin_function)(char **args, char **front);
 
 	while (1)
 	{
-		Firstwrite = write(1, "($) ", 4);
-		if (Firstwrite < 0)
-			perror("write failed");
-		read = _getline(&inputstr, &len, stdin);
-		if (read != -1)
-		{
-			y = 0;
-			fraginputstr[y] = _strtok(inputstr, delim);
-			while (fraginputstr[y] != NULL)
-			{
-				y++;
-				fraginputstr[y] = _strtok(NULL, delim);
-			}
-		}
-		else /* Check for custom EOF i.e Crtl+D */
-		{
-			free(inputstr);
-			write(1, "\n", 1);
-			exit(0);
-		}
+		_prompt(inputstr, fraginputstr);
 		if (fraginputstr[0] != NULL)
 		{
 			if (_strcmp(fraginputstr[0], "exit") == 0)
@@ -92,4 +70,34 @@ int create_process(char *fraginputstr[], char *const envp[])
 		wait(NULL);
 
 	return (0);
+}
+
+char **_prompt(char *inputstr, char *fraginputstr[])
+{
+	size_t len = 100;
+	char delim[] = " \n\t";
+	int y = 0;
+	ssize_t Firstwrite, read = 1;
+	Firstwrite = write(1, "($) ", 4);
+
+		if (Firstwrite < 0)
+			perror("write failed");
+		read = _getline(&inputstr, &len, stdin);
+		if (read != -1)
+		{
+			y = 0;
+			fraginputstr[y] = _strtok(inputstr, delim);
+			while (fraginputstr[y] != NULL)
+			{
+				y++;
+				fraginputstr[y] = _strtok(NULL, delim);
+			}
+		}
+		else /* Check for custom EOF i.e Crtl+D */
+		{
+			free(inputstr);
+			write(1, "\n", 1);
+			exit(0);
+		}
+		return (fraginputstr);
 }
