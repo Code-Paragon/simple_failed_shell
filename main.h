5817db90be5b1b484fd26b84f572e81de0779319
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-08-24 14:29:38 +0100

    betty

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H 
+#define MAIN_H
 
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -15,9 +15,9 @@
 
 extern char **environ;
 
-int shell(char *const envp[], char *inputstr,char **args, char **front);
+int shell(char *const envp[], char *inputstr, char **args, char **front);
 char *handle_path(char *command);
-int execute (char *fraginputstr[], char *const envp[]);
+int execute(char *fraginputstr[], char *const envp[]);
 char **_getenv(char *name);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_strcpy(char *dest, char *src);
@@ -27,10 +27,10 @@ int _strcmp(char *s1, char *s2);
 char *_strchr(char *s, char c);
 char *_strdup(char *str);
 ssize_t _getline(char **inputstr, size_t *m, FILE *stream);
-int com_cd(char **args, char __attribute__((__unused__)) **front);
-int com_env(char **args, char __attribute__((__unused__)) **front);
-int com_setenv(char **args, char __attribute__((__unused__)) **front);
-int com_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int com_cd(char **args, char __attribute__((__unused__)) * *front);
+int com_env(char **args, char __attribute__((__unused__)) * *front);
+int com_setenv(char **args, char __attribute__((__unused__)) * *front);
+int com_unsetenv(char **args, char __attribute__((__unused__)) * *front);
 char *_strtok(char *str, const char *delim);
 int _atoi(char *s);
 int _strncmp(const char *s1, const char *s2, size_t n);
@@ -43,8 +43,8 @@ int shell_exit(char **args);
 typedef struct Plugin_s
 {
 	char *comName;
-	int(*comFunc)(char **argv, char **front);
-}Plugin_t;
+	int (*comFunc)(char **argv, char **front);
+} Plugin_t;
 /**
  * struct dirList_s - A new struct type defining a linked list.
  * @direc: A directory path.
@@ -54,7 +54,7 @@ typedef struct dirList_s
 {
 	char *direc;
 	struct dirList_s *dirNext;
-}dirList_t;
+} dirList_t;
 
 int (*get_Plugin(char *command))(char **args, char **front);
 
